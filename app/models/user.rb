d9726cd8e2a4911c47721ab90b8add105860b683
Egarvey <egarvey.bk@gmail.com>
2025-02-03 12:48:21 -0500

    feat: integrate devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Your existing associations
+  has_many :shopping_lists, dependent: :destroy
+  has_many :list_items, through: :shopping_lists
 end
