c8113aa8831fbb64b4c9c8ef273667f511a6827d
Egarvey <egarvey.bk@gmail.com>
2025-02-04 15:31:34 -0500

    test: fix material detection test count

--- test/models/material_test.rb
+++ test/models/material_test.rb
@@ -12,17 +12,16 @@ class MaterialTest < ActiveSupport::TestCase
     assert_respond_to @material, :material_category
   end
 
-  test "should have many list items" do
+  test "should have one price" do
+    assert_respond_to @material, :price
+  end
+
+  test "should have many list_items" do
     assert_respond_to @material, :list_items
   end
 
   test "should have many shopping lists through list items" do
     assert_respond_to @material, :shopping_lists
-  end
-
-  # Price relationship tests
-  test "should have price relationships" do
-    assert_respond_to @material, :price
   end
 
   # Validation tests
@@ -33,6 +32,7 @@ class MaterialTest < ActiveSupport::TestCase
   test "name should be present" do
     @material.name = nil
     assert_not @material.valid?
+    assert_includes @material.errors[:name], "can't be blank"
   end
 
   test "material_category should be present" do
@@ -41,10 +41,16 @@ class MaterialTest < ActiveSupport::TestCase
     assert_includes @material.errors[:material_category], "must exist"
   end
 
-  test "should destroy associated list items when destroyed" do
-    initial_count = @material.list_items.count
-    new_list_item = @material.list_items.create!(quantity: 1, shopping_list: shopping_lists(:one))
-    assert_difference('ListItem.count', -(initial_count + 1)) do
+  test "should destroy associated price when destroyed" do
+    assert_difference('Price.count', -1) do
+      @material.destroy
+    end
+  end
+
+  test "should destroy associated list_items when destroyed" do
+    list_item_count = @material.list_items.count  # Count list items for this material
+    
+    assert_difference('ListItem.count', -list_item_count) do
       @material.destroy
     end
   end
